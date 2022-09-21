01873d1dfe2128dc84156aa98f7c00c36c22a88a
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2022-09-21 21:58:39 +0500

    finished all function, made work with class VK methods.

--- main.py
+++ main.py
@@ -19,72 +19,73 @@ class VK:
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
 
-    def users_info(self):
-        url = 'https://api.vk.com/method/users.get'
-        params = {'user_ids': self.id}
-        response = requests.get(url, params={**self.params, **params})
-        return response.json()
-
     def get_photos_dict(self):
         url = 'https://api.vk.com/method/photos.get'
         params = {
             'owner_id': self.id,
-            'album_id': 'wall',
+            'album_id': 'profile',
             'rev': 0,
             'extended': 1,
             }
         response = requests.get(url, params={**self.params, **params})
         return response.json()
 
+    def _convert_time(self, some_time):
+        timestamp = int(some_time)
+        res = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+        return res
 
-def convert_time(some_time):
-    timestamp = int(some_time)
-    res = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
-    return res
+    def get_max_photo_list(self):
+        result = self.get_photos_dict()
+        max_photo_list = []
+        for item in result['response']['items']:
+            photo_dict = {'file_name': f"{item['likes']['count']}.jpg"}
+            the_biggest = 0
+            for photo_size in item['sizes']:
+                if photo_size['height'] > the_biggest:
+                    the_biggest = photo_size['height']
+                    photo_dict['size'] = photo_size['type']
+                    photo_dict['link'] = photo_size['url']
+            max_photo_list.append(photo_dict)
 
+            for i in range(len(max_photo_list)):
+                counter = 0
+                for j in range(len(max_photo_list)):
+                    if max_photo_list[i]['file_name'] == max_photo_list[j]['file_name']:
+                        counter += 1
+                if counter > 1:
+                    res = self._convert_time(item['date'])
+                    max_photo_list[i].setdefault('date', f"{res}")
 
-def get_max_photo_list():  # узнать как сделать метод из этой функции.
-    result = vk.get_photos_dict()
-    max_photo_list = []
-    for item in result['response']['items']:
-        photo_dict = {'file_name': f"{item['likes']['count']}.jpg"}
-        links_dict = {}
-        the_biggest = 0
-        for photo_size in item['sizes']:
-            if photo_size['height'] > the_biggest:
-                the_biggest = photo_size['height']
-                photo_dict['size'] = photo_size['type']
-                photo_dict['link'] = photo_size['url']
-        max_photo_list.append(photo_dict)
+        return max_photo_list
 
-        for i in range(len(max_photo_list)):
-            counter = 0
-            for j in range(len(max_photo_list)):
-                if max_photo_list[i]['file_name'] == max_photo_list[j]['file_name']:
-                    counter += 1
-            if counter > 1:
-                max_photo_list[i].setdefault('date', f"{convert_time(item['date'])}")
+    def get_send_list(self):
+        var = self.get_max_photo_list()
+        for item in var:
+            del item['link']
+        return var
 
-    return max_photo_list
+    def _get_json_file(self):
+        with open('photos.json', 'w') as res:
+            json.dump(self.get_send_list(), res, indent=4)
 
+    def _send_json(self, yadisk_file_path):
+        self._get_json_file()
+        upload_file_to_disc(yadisk_file_path)
 
-def get_send_list():
-    var = get_max_photo_list()
-    for item in var:
-        del item['link']
-    return var
+    def _send_photo(self):
+        photo_list = self.get_max_photo_list()
+        upload_photo(photo_list)
 
-
-def get_json_file(some_list):
-    with open('photos.json', 'w') as res:
-        json.dump(some_list, res, indent=4)
-        print('Файл записан.')
+    def send_all(self, yadisk_file_path):
+        self._send_json(yadisk_file_path)
+        self._send_photo()
 
 
 # часть с Яндекс Диском.
 
 
-def get_upload_link(yadisk_file_path):
+def _get_upload_link(yadisk_file_path):
     """This function can get upload link from Yandex Disk"""
     upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
     headers = {'Content_Type': 'application/json', 'Authorization': f'OAuth {yandex_token}'}
@@ -96,9 +97,9 @@ def get_upload_link(yadisk_file_path):
     return response.json()  # возврат ссылки в виде словаря JSON.
 
 
-def upload_file_to_disc(yadisk_file_path, file_name):
+def upload_file_to_disc(yadisk_file_path, file_name='photos.json'):
     """This function can upload chosen file to yandex disk"""
-    upload_link = get_upload_link(yadisk_file_path=yadisk_file_path)  # получаем ссылку в виде json
+    upload_link = _get_upload_link(yadisk_file_path=yadisk_file_path)  # получаем ссылку в виде json
     href = upload_link.get('href', '')  # получаем обяз. параметр href для метода put, что ниже)
     response = requests.put(href, data=open(file_name, 'rb'))  # делаем запрос, указываем href и
     # переменную/открытый файл -
@@ -106,13 +107,12 @@ def upload_file_to_disc(yadisk_file_path, file_name):
     pprint(f'Отправка: {response.status_code}')
 
 
-def upload_photo():
+def upload_photo(some_json):
     url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
     headers = {'Content_Type': 'application/json',
                'Authorization': f'OAuth {yandex_token}'}
 
-    photo_list = get_max_photo_list()
-    # pprint(photo_list)
+    photo_list = some_json
     for item in photo_list:
         response = requests.post(
             url=url,
@@ -130,18 +130,5 @@ if __name__ == "__main__":
     user_id = '607170157'
 
     vk = VK(access_token, user_id)
-    # photos_1 = vk.get_photos_dict()
-    # pprint(photos_1)
-
-    photos = get_max_photo_list()
-    pprint(photos)
-
-    # json_ = get_send_list()
-    # pprint(json_)
-
-    # get_json_file(json_)
-
-    # upload_file_to_disc(f"For course project/photo_JSON", 'photos.json')
-    # upload_photo()
-
+    vk.send_all('For course project/photo_JSON')
 
